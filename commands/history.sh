c3adb6123a74120ecbe3eddf07075dee58c1882f
Joao Almeida <jcfd.almeida@campus.fct.unl.pt>
2021-05-02 13:03:45 +0100

    web url

--- commands/history.sh
+++ commands/history.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source .weburl.env
+
 #Requests the blockchain history for the group $1
 echo "Getting the blockchain history for the group $1... The output can get a little messy."
 curl -X POST -H "Content-Type: application/json" -d "{\"Author\":\"0000\",\"Group\":\"${1}\",\"Commit\":\"0000\"}" ${WEB_URL}/history 
