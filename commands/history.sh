e358e8f2ca593c274b1054c15b615d556cfa47cf
Joao Almeida <jcfd.almeida@campus.fct.unl.pt>
2021-05-09 19:02:53 +0100

    fix #2

--- commands/history.sh
+++ commands/history.sh
@@ -4,4 +4,4 @@ source .weburl.env
 
 #Requests the blockchain history for the group $1
 echo "Getting the blockchain history for the group $1... The output can get a little messy."
-curl -X POST -H "Content-Type: application/json" -d "{\"Author\":\"0000\",\"Group\":\"${1}\",\"Commit\":\"0000\"}" ${WEB_URL}/history 
+curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"0000\",\"Group\":\"${1}\",\"Commit\":\"0000\"}" ${WEB_URL}/history 
