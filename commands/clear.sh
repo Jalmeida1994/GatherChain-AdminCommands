39e53a0cb7fd99c608e2093d74587f5f153b8c0a
Joao Almeida <jcfd.almeida@campus.fct.unl.pt>
2021-05-30 19:21:20 +0100

    fixes

--- commands/clear.sh
+++ commands/clear.sh
@@ -4,8 +4,10 @@
 
 source .weburl.env
 
+pass=${1}
+
 #Requests to create the network with the first group 
 echo "Deleting everything in the the Blockchain Network and bringing it down. This may take a while..."
-curl -X POST -H "Content-Type: application/json" -d "{\"${1}\":\"\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/clear 
+curl -X POST -H "Content-Type: application/json" -d "{\"${pass}\":\"\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/clear 
 
 printf "Network deleted!"
--- commands/init.sh
+++ commands/init.sh
@@ -4,10 +4,12 @@
 
 source .weburl.env
 
+pass=${1}
+
 #Requests to create the network with the first group 
 echo "Standing up the Blockchain Network. This may take a while... Better go brew a coffee and take a seat."
 
-if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${1}\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/init; then
+if curl --fail -X POST -H "Content-Type: application/json" -d "{\"Author\":\"${pass}\",\"Group\":\"0000\",\"Commit\":\"0000\"}" ${WEB_URL}/init; then
 printf "Network launched!"
 else
 printf "Error launching the network."
